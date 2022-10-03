f484b7972233849de35fbcf1c0f81b879bd95f43
kawtharnaser <kawtharnasser7@gmail.com>
2022-10-03 16:56:15 +0300

    idk what im doing, messier circles

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let emptyBoard = document.getElementsByClassName("board")
 let circlesContainer = document.querySelector(".circlesContainer")
-// let circleElement = document.getElementsByClassName("circleElement")
+
+let player1Turn = true
+
+
+
+
+
+
 
 
 
@@ -8,31 +15,71 @@ const generateCircleElements =()=>{
     console.log("generating circles")
     //generate 42 circles
     for(let i=0; i<42; i++){
-
+        
         //step 1: create a new div
         let circleElement = document.createElement("div")
-        console.log(circleElement)
+        let imgElement = document.createElement("img")
         //step 2: give it an id
         circleElement.id = i
-
         //step3: give it a class (style)
+        // circleElement.src="circleElement.svg"
+        // circleElement.style.backgroundRepeat = 'no-repeat'
+        // circleElement.style.backgroundImage="url('./circleElement.svg')"
+        // circleElement.style.backgroundSize="contain"
         circleElement.classList.add("circleElement")
+        imgElement.src="./circleElement.svg"
+        imgElement.style.height = '13%'
+        imgElement.style.width = '13%'
+        imgElement.id = i
+        circleElement.appendChild(imgElement)
+
+        // circleElement.style.marginRight = "30px"
+        imgElement.addEventListener("click", clickCircle)
+        console.log(circleElement)
+        
         // step4: append it to an existing element (its container)
         circlesContainer.appendChild(circleElement)
 
-        //step5: create a new element: an image
-        let circleImg = document.createElement("img")
-        //step6: give the image element a url of the desired image
-        circleImg.src = "circleElement.svg"
-        //step6: append it to an existig element (its div)
-        circleElement.appendChild(circleImg)
-
-
-        if(i%7==0){
-            circleElement.style.display = inline-block
-        }
 
     }
 }
 
+function clickCircle(event){
+    console.log("bloop")
+    // if (player1Turn == true)
+    // {
+
+    // }
+
+     event.target.src = "./pinkCircle.svg"
+     console.log(event.target.id)
+}
+
+
+
+
+
+
+
+
+
+
+
+console.log(circlesContainer)
+
+
+//CODE STARTS HERE 
+//waiting for dom to load before executing code
+document.addEventListener('DOMContentLoaded', ()=>{
+//Adding a function to generate all circles in the board through loop
 generateCircleElements()
+//adding an event listener to listen for clicks on circle elements
+// for(let i=0; i<circlesContainer.length; i++){
+//     circlesContainer[i].addEventListener("click", clickCircle)
+// }
+
+    // circlesContainer.addEventListener("click", clickCircle)
+
+
+
+})
